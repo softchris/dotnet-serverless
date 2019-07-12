5393eeb48d4a832bcb5f35a642a8088c70e1f0b6
Anthony Chu <antchu@microsoft.com>
2019-07-12 12:05:39 -0700

    Remove unecessary usings

--- Db.cs
+++ Db.cs
@@ -1,9 +1,9 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Company {
-  public class Product
+namespace Company
+{
+    public class Product
   {
     public int Id { get; set; }
     public string Name { get; set; }
--- ProductsGet.cs
+++ ProductsGet.cs
@@ -1,17 +1,11 @@
-using System;
-using System.IO;
-using System.Linq;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace Company.Function
-{    
+{
     public static class ProductsGet
     {
         [FunctionName("ProductsGet")]
